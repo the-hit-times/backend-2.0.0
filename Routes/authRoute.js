469edbd8587ffac2d3d58117068c5aa3529e11ec
prabal-github <prabal.chow09009.pc@gmail.com>
2022-10-22 18:21:31 +0530

    image problem fixed

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -1,81 +1,79 @@
 const express = require('express');
 const mongoose = require("mongoose");
-const authcheak= require("../middleware/authcheak")
-const User= require("../mongoSchema/userSchema");
-const jwt= require("jsonwebtoken")
+const authcheak = require("../middleware/authcheak")
+const User = require("../mongoSchema/userSchema");
+const jwt = require("jsonwebtoken")
 
 const bcrypt = require("bcrypt")
 const Router = express.Router();
 
-//register routes////////////////////////
+/* ------------------------------ register routes ------------------------------ */
 
-Router.post("/register",(req,res)=>{
-    req.body.password=bcrypt.hashSync(req.body.password, 10);
-    const new_user=new User(req.body)
-  // console.log(req.body)
-   User.findOne({email:req.body.email}).then(result=>{
-        if(result){
-            res.json({msg:"user already exist"})
+Router.post("/register", (req, res) => {
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
+    const new_user = new User(req.body)
+    // console.log(req.body)
+    User.findOne({ email: req.body.email }).then(result => {
+        if (result) {
+            res.json({ msg: "user already exist" })
         }
-        else{
-            new_user.save().then(async (result)=>{
-                const user=await User.findOne({email:req.body.email});
+        else {
+            new_user.save().then(async (result) => {
+                const user = await User.findOne({ email: req.body.email });
                 //jwt token creation
-                const token=jwt.sign({_id:user._id},process.env.SECRET_KEY);
-                user.token=token;
-                user.save().then((r)=>{console.log("user updated")})
-                
-                res.json({msg:"user registered succesfully"})
+                const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY);
+                user.token = token;
+                user.save().then((r) => { console.log("user updated") })
+
+                res.json({ msg: "user registered succesfully" })
             })
         }
-    }).catch(err=>{
-        res.json({msg:err.message})
+    }).catch(err => {
+        res.json({ msg: err.message })
     })
 })
 
 
+/* ------------------------------ login routes ------------------------------ */
 
-//login routes///////////////////////////
-
-Router.get("/login",authcheak.authcheakForsignin,(req, res)=>{
-    res.render("signin",{ loginResponse: req.flash('loginmsg') })
+Router.get("/login", authcheak.authcheakForsignin, (req, res) => {
+    res.render("signin", { loginResponse: req.flash('loginmsg') })
 })
 
-Router.post("/api/signin",authcheak.authcheakForsignin,async (req,res)=>{
-    try{
-         const user=await User.findOne({email:req.body.email});
-         if(user){
-             if(bcrypt.compareSync(req.body.password,user.password))
-             {
-                 //jwt token creation
-                 const token=jwt.sign({_id:user._id},process.env.SECRET_KEY);
+Router.post("/api/signin", authcheak.authcheakForsignin, async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (user) {
+            if (bcrypt.compareSync(req.body.password, user.password)) {
+                //jwt token creation
+                const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY);
                 // console.log(token)
-                 //save jwt in cookies
-                 res.cookie('jwtoken', token, { expires: false, httpOnly: true });
-                 req.flash('postmsg', 'you logged in succesfully')
-                 res.redirect('/posts')
-             }
-             else{
+                //save jwt in cookies
+                res.cookie('jwtoken', token, { expires: false, httpOnly: true });
+                req.flash('postmsg', 'you logged in succesfully')
+                res.redirect('/posts')
+            }
+            else {
                 req.flash('loginmsg', 'invalid credentials')
-                 res.redirect('/login')
-             }
-         }
-         else{
+                res.redirect('/login')
+            }
+        }
+        else {
             req.flash('loginmsg', 'invalid credentials')
-             res.redirect('/login')
-         }
-     }
-     catch(err){
-         return res.status(400).json({error: err.message})
-     }
- })
+            res.redirect('/login')
+        }
+    }
+    catch (err) {
+        return res.status(400).json({ error: err.message })
+    }
+})
 
- //logout user///////////////////
+/* ------------------------------ logout user ------------------------------ */
 
- Router.get("/logout",(req,res)=>{
+Router.get("/logout", (req, res) => {
     res.clearCookie("jwtoken");
     res.redirect('/login')
 })
 
 
-module.exports =Router
+module.exports = Router
