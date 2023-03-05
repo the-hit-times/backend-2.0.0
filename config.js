d8be4538ef2340ff2f8d850fe6ef0502436011e5
a2krocks <aakapurv@gmail.com>
2023-03-05 18:09:47 +0530

    Introduced new secret key "GOOGLE_APPLICATION_CREDENTIALS" for Firebase Admin SDK

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ const PORT = isDev ? process.env.DEV_PORT : process.env.PORT;
 const MONGO_URI = isDev
   ? process.env.MONGODB_URI_DEV
   : process.env.MONGODB_URI_PROD;
+const GOOGLE_APPLICATION_CREDENTIALS = isDev ? process.env.GOOGLE_APPLICATION_CREDENTIALS_DEV : process.env.GOOGLE_APPLICATION_CREDENTIALS
 
 console.log("MONGO_URI=>", MONGO_URI);
 console.log("PORT=>", PORT);
@@ -13,4 +14,5 @@ module.exports = {
   ENV,
   PORT,
   MONGO_URI,
+  GOOGLE_APPLICATION_CREDENTIALS
 };
