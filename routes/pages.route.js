baebb85761d07b1f8ba9ff60d5ccc56f5969a03f
a2krocks <aakapurv@gmail.com>
2023-09-13 01:35:07 +0530

    Added option to edit and create teams

--- routes/pages.route.js
+++ routes/pages.route.js
@@ -63,6 +63,40 @@ router.get("/post/edit/:postId", authcheak, async (req, res) => {
 
 
 /** Pages Routes for Teams */
+router.get("/teams/create", async (req, res) => {
+
+    const teamToCode = {
+        "100": "CSE",
+        "101": "IT",
+        "102": "ECE",
+        "103": "AEIE",
+        "104": "EE",
+        "105": "MECH",
+        "106": "CIVIL",
+        "107": "CHEMICAL",
+        "108": "BT/FT+AGL",
+        "109": "CSE CS",
+        "110": "CSE DS",
+        "111": "CSE AIML",
+        "112": "MASTERS",
+    }
+    // find all the team codes that does not have a team
+    const allteams = await Team.find().sort({team_code:1});
+    const allteamcodes = allteams.map(team => team.team_code);
+    const teamcodes = Object.keys(teamToCode).filter(code => !allteamcodes.includes(code));
+
+    const teams = teamcodes.map(code => {
+        return {
+            team_code: code,
+            team_name: teamToCode[code]
+        }
+    })
+
+    console.log(teams);
+
+    res.render("teams/create_team", { teams: teams, teamResponse: req.flash('teammsg') })
+});
+
 router.get("/teams/manage", async (req, res) => {
     const allteams = await Team.find().sort({team_code:1});
     if (allteams) {
